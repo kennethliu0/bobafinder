7049a54593b73bb23106acfd208e08fb4979db95
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2026-01-10 11:07:11 -0800

    Configure fireworks

--- main.py
+++ main.py
@@ -1,6 +1,66 @@
-def main():
-    print("Hello from bobafinder!")
+from dotenv import load_dotenv
+
+from langchain_fireworks import ChatFireworks
+from langchain.agents import create_agent
+from langgraph_swarm import create_handoff_tool, create_swarm
+from langgraph.checkpoint.memory import InMemorySaver
 
 
-if __name__ == "__main__":
-    main()
+
+load_dotenv()
+
+model = ChatFireworks(
+    model="accounts/fireworks/models/minimax-m2p1", 
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+alice = create_agent(
+    model,
+    tools=[
+        add,
+        create_handoff_tool(
+            agent_name="Bob",
+            description="Transfer to Bob",
+        ),
+    ],
+    system_prompt="You are Alice, an addition expert.",
+    name="Alice",
+)
+
+bob = create_agent(
+    model,
+    tools=[
+        create_handoff_tool(
+            agent_name="Alice",
+            description="Transfer to Alice, she can help with math",
+        ),
+    ],
+    system_prompt="You are Bob, you speak like a pirate.",
+    name="Bob",
+)
+
+checkpointer = InMemorySaver()
+workflow = create_swarm(
+    [alice, bob],
+    default_active_agent="Alice"
+)
+app = workflow.compile(checkpointer=checkpointer)
+
+config = {"configurable": {"thread_id": "1"}}
+turn_1 = app.invoke(
+    {"messages": [{"role": "user", "content": "i'd like to speak to Bob"}]},
+    config,
+)
+print(turn_1)
+turn_2 = app.invoke(
+    {"messages": [{"role": "user", "content": "what's 5 + 7?"}]},
+    config,
+)
+print(turn_2)
