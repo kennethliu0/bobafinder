b00108777ee059d5deb6be12a5472d5a49e3db1f
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2026-01-10 16:31:27 -0800

    Add mongo

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables before importing agents
 
 from langgraph_swarm import create_swarm
+from pymongo import MongoClient
+from langgraph.checkpoint.mongodb import MongoDBSaver
 
 # Import agents
 from agents.scout import scout
@@ -11,8 +13,15 @@ from agents.quantitative_analyst import quantitative_analyst
 from agents.niche_finder import niche_finder
 from agents.voice import voice
 
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+checkpointer = MongoDBSaver(client)
+db = client.get_database("langgraph")
+collection = db.get_collection("workflows")
+
 workflow = create_swarm(
     [quantitative_analyst, scout, niche_finder, voice],
     default_active_agent="Location Scout"
 )
-app = workflow.compile()
+app = workflow.compile(checkpointer=checkpointer)
+print(app.invoke({"messages": [{"role": "user", "content": "What is the weather in Tokyo?"}]}))
