3ff741842a2694bbab2303491439b555ec457c71
Kenneth Liu <98987701+kennethliu0@users.noreply.github.com>
2026-01-10 16:43:24 -0800

    Update mongo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()  # Load environment variables before importing agents
 from langgraph_swarm import create_swarm
 from pymongo import MongoClient
 from langgraph.checkpoint.mongodb import MongoDBSaver
+from pymongo.server_api import ServerApi
 
 # Import agents
 from agents.scout import scout
@@ -13,8 +14,8 @@ from agents.quantitative_analyst import quantitative_analyst
 from agents.niche_finder import niche_finder
 from agents.voice import voice
 
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
+MONGO_URI = os.getenv("MONGODB_URI")
+client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 checkpointer = MongoDBSaver(client)
 db = client.get_database("langgraph")
 collection = db.get_collection("workflows")
@@ -24,4 +25,5 @@ workflow = create_swarm(
     default_active_agent="Location Scout"
 )
 app = workflow.compile(checkpointer=checkpointer)
-print(app.invoke({"messages": [{"role": "user", "content": "What is the weather in Tokyo?"}]}))
+config = {"configurable": {"thread_id": "5"}}
+print(app.invoke({"messages": [{"role": "user", "content": "What is the weather in Tokyo?"}]}, config=config))
